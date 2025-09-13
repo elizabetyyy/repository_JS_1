25cdf054a5cc232e8e8c454793cd7890edc5747d
Криворучко Єлизавета <elizabeth.kryvoruchko@krfk.kai.edu.ua>
2025-09-13 17:52:31 +0300

    Add files via upload

---
+++ js2/script2.js
@@ -0,0 +1,21 @@
+var a = 7;
+let b = 'добрий день';
+const c = 4;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+let student = {
+    name: 'Yelyzaveta',
+    age: 18,
+    status: true
+}
+
+student.university = "КРФКДНПДУКАІ";
+console.log(student);
+
+b = 6548;
+console.log(typeof b);
+
+c = 52;
